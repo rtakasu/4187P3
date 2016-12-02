955aef983d0f54cde4a3ea0f7e86ebdaa419d579
Rafael Takasu <rafatakasu@gmail.com>
2016-12-02 01:31:10 +0000

    copying contents of input file into output file

--- hwcopy.cpp
+++ hwcopy.cpp
@@ -15,11 +15,23 @@ int main(int argc, char*argv[]) {
 		vector<string> tokens;
 		copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(tokens));
 
-		// Open input file
+		// Open input and output file
 		ifstream input_file;
+		ofstream output_file;
 		input_file.open(tokens[0]);
+		output_file.open(tokens[1]);
 
-		
+		if (input_file.is_open() && output_file.is_open() ) {
+			
+			string line;			
+	
+			while ( getline( input_file, line) ) {
+				
+				output_file<<line<<"\n";				
+
+			}
+
+		}	
 		
 
 		
